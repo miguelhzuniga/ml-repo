b197ffbc3d1966184bff4c48ccff9c74da29150b
Miguel Zúñiga <miguelh.zuniga@javeriana.edu.co>
2025-03-22 15:34:36 -0500

    clean repo

--- taller_1/lib/PUJ_ML/Model/NeuralNetwork/__init__.py
+++
@@ -1,8 +0,0 @@
-## =========================================================================
-## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
-## =========================================================================
-
-from .ActivationFunctions import *
-from .FeedForward import *
-
-## eof - __init__.py
